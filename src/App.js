1c9f04e385735889d9432e9bdf4164364725333a
simranBhamra <45669456+simranBhamra@users.noreply.github.com>
2021-03-09 15:51:17 -0500

    homepage set up and navbar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Homepage from './Pages/Homepage';  
+import Header from "./Components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highestrated: [],
+      names: [],
+      loading: true
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+   // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  // async handleSubmit(event) {
+  //   event.preventDefault();
+  //   this.setState({
+  //     loading: true,
+  //   })
+  //   await fetch('/getnames/' + this.state.name, {
+  //     method: 'POST'
+  //   });
+  //   this.getNames()
+  // }
+
+
+  getNames() {
+    fetch('http://18.206.168.148:5000/movies/highestrated ')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          highestrated: response
+        })
+        console.log(this.state)
+      })
+  }
+
+  componentDidMount() {
+    this.getNames();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <Header></Header>
+        <Homepage></Homepage>
+        </div>
+        {this.state.highestrated.map((movie, index) => (
+        <div key={movie._id}>
+          <h1>{movie.primaryTitle}!</h1>
+          <p>Rated {movie.avgRating} by {movie.numOfVotes} viewers.</p>
+        </div>
+    ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
+
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
+import customTheme from "./utils/themes";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
