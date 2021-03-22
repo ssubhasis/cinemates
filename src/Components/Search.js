4632c833362b62551d80a2a5a61d037bd7353382
simranBhamra <45669456+simranBhamra@users.noreply.github.com>
2021-03-22 14:04:39 -0400

    Search drop down added - merge

---
+++ src/Components/Search.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import {Button, ButtonGroup, Input} from "@chakra-ui/react"
+import axios from 'axios'
+import Suggestions from '../Components/Suggestions'
+
+// const { API_KEY } = process.env
+const API_URL = 'https://teampolaris.web.illinois.edu/search-movies'
+
+class Search extends Component {
+  state = {
+    query: '',
+    results: [],
+    queryTitleId: ''
+  }
+
+  getInfo = () => {
+    axios.get(`${API_URL}/${this.state.query}`)
+      .then(({ data }) => {
+        this.setState({
+          results: data
+        })
+      })
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      } else if (!this.state.query) {
+      }
+    })
+  }
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    this.state.results.map(r => {
+        if (r.primaryTitle == this.state.query){
+          this.state.queryTitleId= r.titleId
+      };
+    });
+    window.location.href = "https://www.google.com/search?q="+this.state.queryTitleId;
+  }
+
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitHandler}>
+        <Input list="browsers" name="browser" id="browser"
+            style={
+                {
+                    marginTop: "10px",
+                    width: "88%",
+                    height:"50px",
+                    borderRadius: "40px"
+                }
+            }
+            focusBorderColor="orange.400"
+            size="lg"
+            bg="white"
+            placeholder="Search"
+            ref={input => this.search = input}
+            onChange={this.handleInputChange}
+        />
+        <Suggestions results={this.state.results} /><br/>
+        <Button colorScheme="orange" variant="solid" type="submit" onClick={this.onSubmitHandler}>Submit</Button>
+
+      </form>
+    )
+
+  }
+}
+
+export default Search
