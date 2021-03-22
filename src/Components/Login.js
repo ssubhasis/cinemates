1143159328eb4d5c0f2ef9e7d7046da48c6a848b
imitra4 <ishitam.2011@gmail.com>
2021-03-22 09:58:27 -0700

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
+import {Form} from "@chakra-ui/react";
+import {Button, ButtonGroup} from "@chakra-ui/react"
+//import "./Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
